b6369e34d8ee42a55918e0a6642f9deb83b14774
Your NameDavid <davidcnz@gmail.com>
2019-01-29 22:13:49 -0700

    add validation

--- config/passport.js
+++ config/passport.js
@@ -5,9 +5,21 @@ const User = mongoose.model('users');
 const keys = require('../config/keys');
 
 const opts = {};
-opts.jwtFormRequest = ExtractJwt.fromAuthHeaderAsBearerToken;
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = keys.secretOrKey;
 
 module.exports = passport => {
-  passport.use(new JwtStrategy());
+  passport.use(
+    new JwtStrategy(opts, (jwt_payload, done) => {
+      console.log(jwt_payload);
+      User.findById(jwt_payload.id)
+        .then(user => {
+          if (user) {
+            return done(null, user);
+          }
+          return done(null, false);
+        })
+        .catch(err => console.log(err));
+    })
+  );
 };
