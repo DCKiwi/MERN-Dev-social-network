004a6a9aafa5bf70eeeb9f3b2785ef1491e7e522
Your NameDavid <davidcnz@gmail.com>
2019-02-11 20:29:32 -0700

    start with auth boilerplate

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <div>
+      <footer>Copyright &copy; {new Date().getFullYear()} This App</footer>
+    </div>
+  );
+}
